e496a5fda83922fecb028f144b3da97fb6c283f6
moon <shawmakesmagic@gmail.com>
2023-07-28 00:14:19 -0700

    Add chat handling

--- agentcomlink/__init__.py
+++ agentcomlink/__init__.py
@@ -7,11 +7,14 @@ from .files import (
     list_files,
     get_file,
 )
-from .server import start_server, get_server, set_storage_path
+from .server import start_server, get_server, set_storage_path, send_message, register_message_handler, unregister_message_handler
 
 __all__ = [
     "get_storage_path",
     "start_server",
+    "send_message",
+    "register_message_handler",
+    "unregister_message_handler",
     "get_server",
     "set_storage_path",
     "add_file",
--- agentcomlink/server.py
+++ agentcomlink/server.py
@@ -6,22 +6,28 @@ from pydantic import BaseModel
 
 from agentcomlink.constants import app, storage_path
 from agentcomlink.files import check_files, get_storage_path, set_storage_path
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 router = APIRouter()
+app = FastAPI()
+
 
 class FilePath(BaseModel):
     path: str
 
+
 def get_server():
     global app
     return app
+
 
 def start_server(storage_path=None, port=8000):
     global app
     if storage_path:
         set_storage_path(storage_path)
     check_files()
-    app = FastAPI()
     app.include_router(router)
     app.mount(
         "/", StaticFiles(directory="static", html=True), name="static"
@@ -33,43 +39,49 @@ def start_server(storage_path=None, port=8000):
         os.environ["PORT"] = str(port)
     return app
 
+
 def stop_server():
     global app
     app = None
 
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
-from typing import List
 
-app = FastAPI()
 ws: WebSocket
 
-# Define a chat storage
-chats: List[str] = []
+handlers = []
+
 
 @app.get("/")
 async def get():
     return FileResponse("static/index.html")
 
+
 async def send_message(message):
-    print("sending message", message)
     await ws.send_text(message)
+
+
+def register_message_handler(handler):
+    global handlers
+    handlers.append(handler)
+
+
+def unregister_message_handler(handler):
+    global handlers
+    handlers.remove(handler)
+
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     global ws
     ws = websocket
     await websocket.accept()
-
     try:
         while True:
             data = await websocket.receive_text()
-            print(f"Received message: {data}")
-            await send_message(data)
-            chats.append(data)
+            for handler in handlers:
+                handler(data)
     except WebSocketDisconnect:
         ws = None
+
 
 @router.post("/file/")
 async def http_add_file(path: str = Form(...), file: UploadFile = File(...)):
@@ -77,6 +89,7 @@ async def http_add_file(path: str = Form(...), file: UploadFile = File(...)):
     with open(os.path.join(storage_path, path), "wb") as f:
         f.write(await file.read())
     return {"message": "File created"}
+
 
 @router.delete("/file/{path}")
 def http_remove_file(path: str):
@@ -87,6 +100,7 @@ def http_remove_file(path: str):
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+
 @router.put("/file/")
 async def http_update_file(path: str = Form(...), file: UploadFile = File(...)):
     check_files()
@@ -94,10 +108,12 @@ async def http_update_file(path: str = Form(...), file: UploadFile = File(...)):
         f.write(await file.read())
     return {"message": "File updated"}
 
+
 @router.get("/files/")
 def http_list_files(path: str = "."):
     check_files()
     return {"files": os.listdir(os.path.join(storage_path, path))}
+
 
 @router.get("/file/{path}")
 def http_get_file(path: str):
